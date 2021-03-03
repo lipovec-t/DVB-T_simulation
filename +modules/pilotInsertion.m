c7f8f097a0b1dd8bdd9c4ddfcec3b13f3cb12d18
Thomas Lipovec <thomas.lipovec@tuwien.ac.at>
2021-03-03 22:09:31 +0100

    Adds first draft of the feature pilotInsertion

--- +modules/pilotInsertion.m
+++ +modules/pilotInsertion.m
@@ -1,7 +1,19 @@
 function dataWithPilots = pilotInsertion(dataMod)
-pnSequence = comm.PNSequence('VariableSizeOutput', true, 'MaximumOutputSize', [6817 1]);
-pnSequence.Polynomial = 'z^11 + z^2 + 1';
-pnSequence.InitialConditions = [1 1 1 1 1 1 1 1 1 1 1];
-test = pnSequence(6817);
-    
+% Inserts pilots in the 68x6817 frame grid.
+    nOFDMsymbols = 68;
+    nSubcarriers = 6817;
+    PRBS = comm.PNSequence('VariableSizeOutput', true, 'MaximumOutputSize', [nSubcarriers 1]);
+    PRBS.Polynomial = 'z^11 + z^2 + 1';
+    PRBS.InitialConditions = [1 1 1 1 1 1 1 1 1 1 1];
+    sequence = PRBS(nSubcarriers);
+    pilotPositions = zeros(nOFDMsymbols, nSubcarriers);
+    pilots = zeros(nOFDMsymbols, nSubcarriers);
+    for i=1:nOFDMsymbols
+        for j=(mod(i-1,4)*3+3+1):12:nSubcarriers
+            pilotPositions(i,j) = 1;
+            pilots(i,j) = 4/3 * 2 * (1/2 - sequence(j));
+        end
+    end
+    dataPositions = ones(nOFDMsymbols, nSubcarriers)-pilotPositions;
+    dataWithPilots = dataMod .* dataPositions + pilots;
 end
--- simulate.m
+++ simulate.m
@@ -20,3 +20,4 @@ for i=1:nOFDMsymbols
 end
 
 %% Pilot insertion
+dataModWithPilots = modules.pilotInsertion(dataMod);
