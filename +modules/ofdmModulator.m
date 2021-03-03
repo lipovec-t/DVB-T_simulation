e07abe35d1ee59fbbe337a73ffc0bcc4accd28a2
Thomas Lipovec <thomas.lipovec@tuwien.ac.at>
2021-03-03 23:09:52 +0100

    Implements modulation

--- +modules/ofdmModulator.m
+++ +modules/ofdmModulator.m
@@ -1,6 +1,12 @@
 function ofdmSignal = ofdmModulator(dataModWithPilots)
 % OFDM Modulator
+    nOFDMsymbols = 68;
     FFTsize = 8192;
     CPlength = 8192/4;
     ofdmSignal = zeros(nOFDMsymbols, FFTsize + CPlength);
+    for i=1:nOFDMsymbols
+        ofdmSignal(i,CPlength+1:end) = ifft(dataModWithPilots(i,:), FFTsize);
+        ofdmSignal(i,1:CPlength) = ofdmSignal(i, end-CPlength+1:end);
+    end
+        
 end
