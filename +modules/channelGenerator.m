c124fb9b23684b2ca2553b734a3f4cc0f48c2ec2
Thomas Lipovec <thomas.lipovec@tuwien.ac.at>
2021-06-22 21:16:23 +0200

    Add generation of channel impulse response

--- +modules/channelGenerator.m
+++ +modules/channelGenerator.m
@@ -1,7 +1,11 @@
-function h = channelGenerator(d1, d2, d3)
+function h = channelGenerator()
 % Generate Ricean fading channel (channel impulse response)
-% Input: Tx - RX distances
-    
+
+    % TX - RX distances
+    d1 = 2000;
+    d2 = 10000;
+    d3 = 40000;
+
     % Amplitudes
     A1 = 1 / d1;
     A2 = 1 / d2;
@@ -13,14 +17,14 @@ function h = channelGenerator(d1, d2, d3)
     l3 = 3*d3 / 100;
 
     % decay factors for PDP
-    decay1 = 1.2;
-    decay2 = 1.7;
-    decay3 = 3.1;
+    decay1 = 0.2;
+    decay2 = 0.13;
+    decay3 = 0.4;
 
     % Number of multipaths
-    nMultipaths1 = 40;
-    nMultipaths2 = 33;
-    nMultipaths3 = 72;
+    nMultipaths1 = 132;
+    nMultipaths2 = 314;
+    nMultipaths3 = 169;
 
     % taps per Tx - RX
     tau1 = 0:nMultipaths1;
@@ -35,5 +39,34 @@ function h = channelGenerator(d1, d2, d3)
     PDP3    = exp(-decay3 * tau3);
     PDPamp3 = sqrt(PDP3);
     
-    % Ricean fading
+    % Ricean fading - generated from two Gaussian according to Wikipedia
+    K = 2;
+    s = sqrt(K/(1+K));
+    sigma = sqrt(1/(2*(1+K)));
+    
+    X1 = s + sigma * randn(1,nMultipaths1+1);
+    Y1 = sigma * randn(1,nMultipaths1+1);
+    riceanAmp1 = X1 + 1j*Y1;
+    
+    X2 = s + sigma * randn(1,nMultipaths2+1);
+    Y2 = sigma * randn(1,nMultipaths2+1);
+    riceanAmp2 = X2 + 1j*Y2;
+    
+    X3 = s + sigma * randn(1,nMultipaths3+1);
+    Y3 = sigma * randn(1,nMultipaths3+1);
+    riceanAmp3 = X3 + 1j*Y3;
+    
+    % impulse response
+    h1 = riceanAmp1.*PDPamp1;    
+    h2 = riceanAmp2.*PDPamp2; 
+    h3 = riceanAmp3.*PDPamp3;
+    
+    h_notNormalized = zeros(1,l3+nMultipaths3+1);
+    h_notNormalized(l1:l1+nMultipaths1) = h_notNormalized(l1:l1+nMultipaths1) + h1;
+    h_notNormalized(l2:l2+nMultipaths2) = h_notNormalized(l2:l2+nMultipaths2) + h2;
+    h_notNormalized(l3:l3+nMultipaths3) = h_notNormalized(l3:l3+nMultipaths3) + h3;
+        
+    h = h_notNormalized / sqrt(sum(abs(h_notNormalized).^2));
+    
+    
     
