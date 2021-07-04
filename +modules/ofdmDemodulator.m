973a21a8c7cae9f82112a125e4443f1031b9c5cb
Thomas <63391717+wtfxyz@users.noreply.github.com>
2021-07-04 11:56:54 +0200

    Rework channel estimation

--- +modules/ofdmDemodulator.m
+++ +modules/ofdmDemodulator.m
@@ -4,10 +4,11 @@ function dataRX = ofdmDemodulator(ofdmSignalRXsynchronized)
     FFTsize = 8192;
     CPlength = 8192/4;
     dataRX = zeros(nOFDMsymbols, FFTsize);
+    
     for i=1:nOFDMsymbols
         % Remove CP
         dataRX(i,:) = ofdmSignalRXsynchronized(i,CPlength+1:end);
         % FFT
-        dataRX(i,:) = fft(dataRX(i,:));
+        dataRX(i,:) = fft(dataRX(i,:), FFTsize);
     end
 end
