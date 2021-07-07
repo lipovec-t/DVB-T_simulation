530bb521b50d12b41344a3437e4c5ceae104373a
Thomas <63391717+wtfxyz@users.noreply.github.com>
2021-07-07 16:05:49 +0200

    Add second simulation function for testing purposes

---
+++ simulate2.m
@@ -0,0 +1,121 @@
+% Simulation of a rudimentary DVB-T system
+
+% Author: Thomas Lipovec
+% Matriculation number: 01529232
+% Email: thomas.lipovec@tuwien.ac.at
+% March 2021; Last revision: 07-July-2021
+
+% DVB-T Parameters:
+%   FFT Size                    = 8192
+%   Number of used subcarriers  = 6817
+%   Number of data carriers     = 6247
+%   Number of pilots            = 570
+%   Relative CP length          = 1/4
+%   Constellation               = 16-QAM
+
+% 1 OFDM Symbol     = 8192 + 8192/4     = 10240 samples
+% 1 frame           = 68 OFDM symbols   = 696320 samples
+
+
+%% Generate Data
+%rng('default')
+nSamples = 6817;
+kBits = 4; % Bits per sample
+nOFDMsymbols = 68; % per frame
+% Generate vector of random binary data.
+dataIn = randi([0 1], 68, nSamples*kBits);
+
+%% 16-QAM
+dataMod = zeros(nOFDMsymbols, nSamples);
+for i=1:nOFDMsymbols
+    dataMod(i,:) = modules.qamModulation(dataIn(i,:)); 
+end
+
+%% Pilot insertion
+[dataModWithPilots, pilots, pilotPositions] = modules.pilotInsertion(dataMod);
+
+%% OFDM modulator
+ofdmSignalTX = modules.ofdmModulator(dataModWithPilots);
+
+%% Channel simulation
+SNRdB   = 20;
+SNRlin  = 10^(SNRdB/10);
+% transmit data over channel
+channelLength = length(modules.channelGenerator);
+channel = zeros(68,channelLength);
+% ofdmSignalRX1 corresponds to a frame of OFDM symbols
+convLength = 10240 + channelLength - 1;
+ofdmSignalRX1 = zeros(68, 10240);% + channelLength - 1);
+ofdmSignalRX2 = zeros(1, 68*10240);
+
+for i=1:nOFDMsymbols
+    signalTX = ofdmSignalTX(i,:);
+    signalPower = sum(abs(signalTX).^2) / length(signalTX);
+    noisePower = signalPower / SNRlin;
+    % convolution with channel impulse response
+    channel(i,:) = modules.channelGenerator();
+    RXdataNoNoise = conv(signalTX,channel(i,:));
+    n = sqrt(noisePower/2) * (randn(1,length(RXdataNoNoise)) + 1j*randn(1,length(RXdataNoNoise)));
+    RXdata1 = RXdataNoNoise + n;
+    if i==1
+        ofdmSignalRX2(1:10240) = ofdmSignalRX2(1:10240)+RXdata1(channelLength:end);
+    elseif i==nOFDMsymbols
+        ofdmSignalRX2(end-10240+1:end) =ofdmSignalRX2(end-10240+1:end)+ RXdata1(1:end-channelLength+1);
+    else
+        ofdmSignalRX2((i-1)*(10240-channelLength)+1:(i-1)*(10240-channelLength)+convLength) =ofdmSignalRX2((i-1)*(10240-channelLength)+1:(i-1)*(10240-channelLength)+convLength)+ RXdata1;
+    end
+end
+ofdmSignalRX1 = reshape(ofdmSignalRX2,10240,68)';
+
+% time and frequency offset
+timeOffset = randi([0,600],1);
+frequencyOffsetMin = -1/2;
+frequencyOffsetMax = 1/2;
+frequencyOffset = (frequencyOffsetMax - frequencyOffsetMin) * rand() + frequencyOffsetMin;
+ofdmSignalRXdelayed = [zeros(1,timeOffset), ofdmSignalRX2];
+ofdmSignalRXdelayed(1:timeOffset) = sqrt(noisePower/2) * (randn(1,timeOffset) + 1j*randn(1,timeOffset));
+m = 0:1:length(ofdmSignalRXdelayed)-1;
+ofdmSignalRX = ofdmSignalRXdelayed .* exp(1i*2*pi*frequencyOffset*m/8192);
+
+%% Synchronisation
+ofdmSignalRXsynchronized = modules.offsetEstimatorNew(ofdmSignalRX, SNRlin, timeOffset, frequencyOffset);
+%ofdmSignalRXsynchronized = ofdmSignalRX1; % without offset & synchronization
+
+%% Demodulation
+dataRX = modules.ofdmDemodulator(ofdmSignalRXsynchronized);
+% extract data of used subcarriers
+dataRXestimated = dataRX(:,1:6817);
+
+%% Channel Estimation
+% H = modules.channelEstimation(dataRXestimated, pilots);
+
+% assume perfect channel knowledge
+H = zeros(nOFDMsymbols,8192);
+for i=1:nOFDMsymbols
+    H(i,:) = fft(channel(i,:),8192);
+end
+H = H(:,1:6817);
+%----------------------------------
+dataRXestimated = dataRXestimated ./ H;
+
+%% Demapping
+dataDemappedEstimated = modules.symbolDemapping(dataRXestimated);
+% Remove pilots and get serial stream of data
+dataDemappedEstimated = dataDemappedEstimated.';
+dataDemappedEstimated(logical(pilotPositions.')) = [];
+dataOut = de2bi(dataDemappedEstimated, 'left-msb');
+
+%% Compute number of bit errors
+% Get original data stream
+dataCompare = modules.symbolDemapping(dataMod);
+dataCompare = dataCompare.';
+dataCompare(logical(pilotPositions.')) = [];
+dataCompare = de2bi(dataCompare.', 'left-msb');
+
+
+% Compute errors
+errorCount = sum(reshape(dataOut',1,[]) ~= reshape(dataCompare',1,[]));
+errorRatio = errorCount / numel(dataOut);
+
+
+
